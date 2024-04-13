1c73a031198008509441ceefa093acfdd180cc6a
Kanin-McGuire <kamc9940@colorado.edu>
2024-04-13 13:55:06 -0600

    buildroot

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = '988db8e53b93b025d1b7f2d201f17681906c17fc'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-Kanin-McGuire.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -23,6 +23,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
--- shared.sh
+++ shared.sh
@@ -6,6 +6,18 @@ QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
 # The place we store customizations to the qemu configuration
 MODIFIED_QEMU_DEFCONFIG=base_external/configs/aesd_qemu_defconfig
 # The defconfig from the buildroot directory we use for the project
-AESD_DEFAULT_DEFCONFIG=${QEMU_DEFCONFIG}
-AESD_MODIFIED_DEFCONFIG=${MODIFIED_QEMU_DEFCONFIG}
+# AESD_DEFAULT_DEFCONFIG=${QEMU_DEFCONFIG}
+# AESD_MODIFIED_DEFCONFIG=${MODIFIED_QEMU_DEFCONFIG}
+# AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${AESD_MODIFIED_DEFCONFIG}
+
+# The default hardware platform, when building for hardware
+
+
+# The defconfig from the buildroot directory we use for qemu builds
+RASPBERRY_PI_DEFCONFIG=configs/raspberrypi3_defconfig
+# The place we store customizations to the qemu configuration
+MODIFIED_RASPBERRY_PI_DEFCONFIG=base_external/configs/aesd_raspberrypi_defconfig
+
+AESD_DEFAULT_DEFCONFIG=${RASPBERRY_PI_DEFCONFIG}
+AESD_MODIFIED_DEFCONFIG=${MODIFIED_RASPBERRY_PI_DEFCONFIG}
 AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${AESD_MODIFIED_DEFCONFIG}
