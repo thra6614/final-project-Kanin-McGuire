1b1448707dda33db31cc0cf9f6504ce79491bfd2
Kanin-McGuire <kamc9940@colorado.edu>
2024-04-24 16:18:52 -0600

    update for commit change

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '4722600c19e9d11b5097edb36a752ab5cbccae8d'
+AESD_ASSIGNMENTS_VERSION = 'a09935febbe3e57f28827ad61d24d295a301a9b3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,7 +23,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/UserInterface/simulate.py $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/UserInterface/finalProject.db $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/UserInterface/ui.py $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
